96de98f6ab4f5f996fbb05c93b8dac1b47b75632
NahomTadesse <nahitad12@gmail.com>
2020-01-03 14:42:52 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -122,6 +122,24 @@ function nahomtheme_widgets_init() {
         'before_title' => '<h3 class="footer-widget-title">',
         'after_title' => '</h3>',
         ) );
+    register_sidebar( array(
+        'name' => 'Footer Sidebar 2',
+        'id' => 'footer-sidebar-2',
+        'description' => 'Appears in the footer area',
+        'before_widget' => '<aside id="%1$s" class="footer-widget %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="footer-widget-title">',
+        'after_title' => '</h3>',
+            ) );
+    register_sidebar( array(
+        'name' => 'Footer Sidebar 3',
+        'id' => 'footer-sidebar-3',
+        'description' => 'Appears in the footer area',
+        'before_widget' => '<aside id="%1$s" class="footer-widget %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="footer-widget-title">',
+        'after_title' => '</h3>',
+            ) );
 }
 add_action( 'widgets_init', 'nahomtheme_widgets_init' );
 
