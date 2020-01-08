6224bc6d7ec946d9432a00c9da1e182a38cde61b
NahomTadesse <nahitad12@gmail.com>
2020-01-08 11:33:48 +0300

    css and header  changes

--- header.php
+++ header.php
@@ -50,25 +50,10 @@ document.querySelector("button").addEventListener("click",function()){document.q
 </div>
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
-			<?php
+			 <?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$nahomtheme_description = get_bloginfo( 'description', 'display' );
-			if ( $nahomtheme_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $nahomtheme_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
+			?>
 		</div><!-- .site-branding -->
-
-
 
 	</header><!-- #masthead -->
 
